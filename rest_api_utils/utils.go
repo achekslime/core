3ab3cbfa605106e9b5e7622e0c86c7b21989fdfd
Asatiani Timur <impsface@yandex.ru>
2023-05-07 06:24:43 +0300

    extract token

--- rest_api_utils/utils.go
+++ rest_api_utils/utils.go
@@ -1,9 +1,23 @@
 package rest_api_utils
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
+
+func ExtractToken(context *gin.Context) (string, error) {
+	token := context.Query("token")
+	if token != "" {
+		return token, nil
+	}
+	bearerToken := context.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1], nil
+	}
+	return "", errors.New("invalid token")
+}
 
 func BindBadRequest(context *gin.Context, err error) {
 	context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
