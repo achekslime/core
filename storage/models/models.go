e0d7455d87d39e00f10bfb5cc3ca994a5928fc8d
Asatiani Timur <impsface@yandex.ru>
2023-05-07 05:37:12 +0300

    models transformation

--- storage/models/models.go
+++ storage/models/models.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"github.com/achekslime/core/models"
+)
+
 type User struct {
 	ID       int    `json:"id" db:"id"`
 	Email    string `json:"email" db:"email"`
@@ -10,4 +14,18 @@ type Room struct {
 	ID      int    `json:"id" db:"id"`
 	Name    string `json:"name" db:"name"`
 	AdminID int    `json:"admin_id" db:"admin_id"`
+}
+
+func UserToDbUser(user *models.User) *User {
+	return &User{
+		Email:    user.Email,
+		Password: user.Password,
+	}
+}
+
+func RoomToDbRoom(room *models.Room, adminID int) *Room {
+	return &Room{
+		Name:    room.Name,
+		AdminID: adminID,
+	}
 }
