d968e46b4d9edc45a9df9308feddea06aa440e6b
Asatiani Timur <impsface@yandex.ru>
2023-05-15 05:08:53 +0300

    postgres err

--- storage/postgres/common.go
+++ storage/postgres/common.go
@@ -4,4 +4,7 @@ const (
 	UserTableName           = "client"
 	RoomTableName           = "room"
 	AvailableRoomsTableName = "available_rooms"
+
+	errUniqueConstraintDuplicate = "duplicate key value violates unique constraint"
+	errSqlNoRows                 = "sql: no rows in result set"
 )
